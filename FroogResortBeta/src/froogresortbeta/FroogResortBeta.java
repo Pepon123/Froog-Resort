b1d1b4cb054908ab3c05805706c9c29e051620f5
Jose Cordero <Jose Cordero@PEPE2>
2021-05-12 17:53:05 -0500

    Modificaciones

---
+++ FroogResortBeta/src/froogresortbeta/FroogResortBeta.java
@@ -0,0 +1,12 @@
+//Bienvenido a mi Hotel:)
+package froogresortbeta;
+
+
+public class FroogResortBeta {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
